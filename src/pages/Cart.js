01e838122ddf259db1fd740d569cf493b68d3e7a
GuQli <gklibadze@gmail.com>
2023-06-16 20:26:50 +0400

    second commit

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,5 +1,30 @@
 import React from "react";
+import { CartCard } from "../components/CartCard";
 
 export const Cart = () => {
-  return <div>Cart</div>;
+  const cartList = [
+    {
+      id: 1,
+      name: "headphone example1",
+      image: "/assets/images/1001.png",
+      price: "10",
+    },
+    {
+      id: 2,
+      name: "headphone example2",
+      image: "/assets/images/1002.png",
+      price: "102",
+    },
+  ];
+
+  return (
+    <main>
+      <section className="cart">
+        <h1>Cart Items: {cartList.length}</h1>
+        {cartList.map((product) => (
+          <CartCard key={product.id} product={product} />
+        ))}
+      </section>
+    </main>
+  );
 };
