8df7744a40f06abaca8424b489c7ab282d8819f3
GuQli <gklibadze@gmail.com>
2023-06-16 20:02:15 +0400

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { ProductCard } from "../components/ProductCard";
+
+export const Home = () => {
+  const products = [
+    {
+      id: 1,
+      name: "headphone example1",
+      image: "/assets/images/1001.png",
+      price: "10",
+    },
+    {
+      id: 2,
+      name: "headphone example2",
+      image: "/assets/images/1002.png",
+      price: "102",
+    },
+    {
+      id: 3,
+      name: "headphone example3",
+      image: "/assets/images/1003.png",
+      price: "120",
+    },
+    {
+      id: 4,
+      name: "headphone example4",
+      image: "/assets/images/1004.png",
+      price: "130",
+    },
+    {
+      id: 5,
+      name: "headphone example4",
+      image: "/assets/images/1005.png",
+      price: "130",
+    },
+    {
+      id: 6,
+      name: "headphone example4",
+      image: "/assets/images/1006.png",
+      price: "130",
+    },
+  ];
+
+  return (
+    <main>
+      <section className="products">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </section>
+    </main>
+  );
+};
